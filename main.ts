50aa75385ffa00a0d7cf2af92c45c7577ebd1d5e
threeup <threeup@gmail.com>
2021-02-20 17:56:38 -0800

    letsgo

--- main.ts
+++ main.ts
@@ -1,3 +1,68 @@
+
+enum Continent {
+    NorthAmerica,
+    SouthAmerica,
+    Europe,
+    Africa,
+    Asia,
+    Oceania,
+    Nothing,
+}   // enum Continent
+
 scene.setBackgroundColor(1)
-let mySprite = sprites.create(background1.wholemap, 0)
-mySprite.startEffect(effects.starField)
+let mapSprite = sprites.create(background1.wholemap, 0);
+mapSprite.setPosition(80, 50)
+
+let cursor: Sprite = null;
+cursor = sprites.create(entities.hand);
+cursor.setPosition(30,30);
+
+let label: TextSprite = null
+label = textsprite.create("NorthAmerica", 0, 1)
+label.z = 1
+label.setOutline(3, 6)
+
+function findClosestContinent(x:number, y:number):Continent {
+    if(x < 60) {
+        return y < 60?Continent.NorthAmerica:Continent.SouthAmerica;
+    } else if (x<110) {
+        return y < 57?Continent.Europe:Continent.Africa;
+    } else {
+        return y < 75?Continent.Asia:Continent.Oceania;
+    }
+    
+
+}
+
+game.onUpdate(function() {
+    let vala = controller.A.isPressed();
+    let valb = controller.B.isPressed();
+    let u = controller.up.isPressed();
+    let d= controller.down.isPressed();
+    let r = controller.right.isPressed();
+    let l = controller.left.isPressed();
+
+    if(u) {
+        cursor.setVelocity(0, cursor.vy - 2);
+    } else if(d) {
+        cursor.setVelocity(0, cursor.vy + 2);
+    } else if(r) {
+        cursor.setVelocity(cursor.vx + 2, 0);
+    } else if(l) {
+        cursor.setVelocity(cursor.vx - 2, 0);
+    } else {
+        cursor.setVelocity(0, 0);
+    } 
+    
+    label.setPosition(cursor.x+14, cursor.y + 11)
+    let closest = findClosestContinent(cursor.x, cursor.y);
+    switch(closest) {
+        default: break;
+        case Continent.NorthAmerica: label.setText("North America"); break;
+        case Continent.SouthAmerica: label.setText("South America"); break;
+        case Continent.Europe: label.setText("Europe"); break;
+        case Continent.Africa: label.setText("Africa"); break;
+        case Continent.Asia: label.setText("Asia"); break;
+        case Continent.Oceania: label.setText("Oceania"); break;
+    }
+})
